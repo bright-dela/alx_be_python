b170a777353106a682a0e1ca5c30903550d79db6
Bright Settor <brightdelasettor@gmail.com>
2025-07-07 11:27:02 +0000

    Initial commit

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,12 @@
+number1 = 10
+number2 = 5
+
+
+add_numbers = number1 + number2
+print("Addition of", number1, "and", number2, "is", add_numbers)
+
+subtract_numbers = number1 - number2
+print("Subtraction of", number1, "and", number2, "is", subtract_numbers)
+
+multiply_numbers = number1 * number2
+print("Multiplication of", number1, "and", number2, "is", multiply_numbers)
