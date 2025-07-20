a21ab7c6c823b37d1466e8bd612ae1c61480054d
bright-settor <brightdelasettor@gmail.com>
2025-07-20 20:25:29 +0000

    Add pattern_drawing file

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,16 @@
+pattern_size = int(input("Enter the size of the pattern: "))
+
+x = pattern_size
+
+while x > 0:
+
+    x -= 1
+
+    for size in range(pattern_size):
+        print("*", end="")
+
+    print("")
+        
+    
+   
+
