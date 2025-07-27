c2a4c52b544385d03c1f0a854f605ee8fb1ff722
bright-settor <brightdelasettor@gmail.com>
2025-07-27 16:31:58 +0000

    Update input prompt for shopping_list_manager file

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -14,7 +14,7 @@ def main():
 
         if choice == "1":
             # Prompt for and add an item
-            add_item = input("Enter item to your list: ")
+            add_item = input("Enter the item to add: ")
             shopping_list.append(add_item)
 
         elif choice == "2":
