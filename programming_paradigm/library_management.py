6a378797f734398b76cec35d91c48957011912ff
bright-settor <brightdelasettor@gmail.com>
2025-08-01 19:00:00 +0000

    Refactor code in library management files

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -1,12 +1,20 @@
 class Book:
-
-    def __init__(self, title, author, is_checked_out=False):
+    def __init__(self, title, author):
         self.title = title
         self.author = author
-        self._is_checked_out = is_checked_out
+        self._is_checked_out = False
 
     def __str__(self):
         return f"{self.title} by {self.author}"
+
+    def is_checked_out(self):
+        return self._is_checked_out
+
+    def check_out(self):
+        self._is_checked_out = True
+
+    def return_book(self):
+        self._is_checked_out = False
 
 
 class Library:
@@ -14,18 +22,28 @@ class Library:
         self._books = []
 
     def add_book(self, book):
-        return self._books.append(book)
+        self._books.append(book)
 
     def check_out_book(self, title):
         for book in self._books:
-            if title == book.title:
-                return self._books.remove(book)
+            if book.title == title and not book.is_checked_out():
+                book.check_out()
+                return True
+        return False
 
     def return_book(self, title):
         for book in self._books:
-            if title == book.title:
-                return self._books.append(book)
+            if book.title == title and book.is_checked_out():
+                book.return_book()
+                return True
+        return False
 
     def list_available_books(self):
+        available_books = []
+
         for book in self._books:
+            if book.is_checked_out() == False:
+                available_books.append(book)
+
+        for book in available_books:
             print(book)
